d2bdde4e980792914f297efa0968dc750d041dda
agandia9 <alejandro.gandia9@gmail.com>
2017-07-31 17:13:22 +0200

    done without styles

--- src/App.js
+++ src/App.js
@@ -8,18 +8,26 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      user: null
+      user: null,
+      pictures: []
     }
 
     this.handleAuth = this.handleAuth.bind(this)
     this.handleLogout = this.handleLogout.bind(this)
     this.renderLoginButton = this.renderLoginButton.bind(this)
+    this.handleUpload = this.handleUpload.bind(this)
   }
 
   // WHEN COMPONENT IS MOUNT...
   componentWillMount(){
     firebase.auth().onAuthStateChanged(user => {
       this.setState({user})
+    })
+
+    firebase.database().ref('pictures').on('child_added', snapshot => {
+      this.setState({
+        pictures: this.state.pictures.concat(snapshot.val())
+      })
     })
   }
 
@@ -38,6 +46,33 @@ class App extends Component {
     .then(result => console.log(`${result.user.email} ha cerrado sesión`))
     .catch(error => console.log(`Error ${error.code}: ${error.message}`))
   }
+
+  handleUpload (event){
+    const file = event.target.files[0]
+    // `/fotos/${file.name}`=> this crash if cancels the upload...
+    const storageRef = firebase.storage().ref(`/fotos/${file.name}`)
+    const task = storageRef.put(file)
+
+    task.on('state_changed', snapshot => {
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes)
+      this.setState ({
+        uploadValue: percentage
+      })
+    }, error => {console.log(error.message)
+    },() => {
+    const record = {
+      photoURL: this.state.user.photoURL,
+      displayName: this.state.user.displayName,
+      image: task.snapshot.downloadURL
+    }
+
+    const dbRef = firebase.database().ref('pictures')
+    const newPicture = dbRef.push()
+    newPicture.set(record)
+  })
+  }
+
+
   // RENDER BUTTON HANDLER
     renderLoginButton() {
       // if user is logged
@@ -47,7 +82,18 @@ class App extends Component {
             <img width="150" height="150" src={ this.state.user.photoURL } alt={ this.state.user.displayName } />
             <p>hola { this.state.user.displayName }!</p>
             <button onClick={this.handleLogout}> Salir </button>
-            <FileUpload />
+            <FileUpload onUpload={this.handleUpload} />
+            {
+              this.state.pictures.map(picture => (
+                <div>
+                  <img width="150" height="150" src={picture.image} />
+                  <br />
+                  <img width="50" height="50" src={picture.photoURL} alt={picture.displayName}/>
+                  <br />
+                  <span>{picture.displayName}</span>
+                </div>
+              )).reverse()
+            }
           </div>
         );
       } else {
